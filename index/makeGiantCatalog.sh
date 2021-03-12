e3cf8938cb89ed61caa40d8e4758eae47b0558c2
Jeremy faden <faden@cottagesystems.com>
2021-03-12 07:51:35 -0600

    automatic update

--- index/makeGiantCatalog.sh
+++ index/makeGiantCatalog.sh
@@ -4,7 +4,7 @@ echo "begin makeGiantCatalog"
 SECONDS=0 # Magic variable
 cd  /home/jbf/ct/git/servers/index
 git pull 
-#java -cp /home/jbf/bin/autoplot.jar -Djava.awt.headless=true org.autoplot.AutoplotUI --script=makeGiantCatalog.jy
+java -cp /home/jbf/bin/autoplot.jar -Djava.awt.headless=true org.autoplot.AutoplotUI --script=makeGiantCatalog.jy
 git commit -m "automatic update" *
 git push
 echo "finished in seconds: " $SECONDS

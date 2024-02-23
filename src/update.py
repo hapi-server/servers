362d6caa75809c3ebe0b8f4b0c50e574fe0f2dc9
Bob Weigel <rweigel@gmu.edu>
2024-02-23 12:31:24 -0500

    near finished update.py

--- src/update.py
+++ src/update.py
@@ -1,36 +1,70 @@
 import json
+import datetime
 import requests
 
 def get_about(about_url):
 
-    # Send a GET request and check for errors
-    try:
-        response = requests.get(about_url)
-        response.raise_for_status()  # Raise an error if response code is not 2xx
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching data: {e}")
-        exit(1)
+  print(f"Fetching {about_url}")
+  try:
+    response = requests.get(about_url)
+    response.raise_for_status()  # Raise an error if response code is not 2xx
+  except requests.exceptions.RequestException as e:
+    print(f"  {e}")
+    return e
+  print(f"  Fetched {about_url}")
 
-    # Parse the JSON data
-    try:
-        data = json.loads(response.text)
-    except json.JSONDecodeError as e:
-        print(f"Error parsing JSON: {e}")
-        exit(1)
+  try:
+    data = json.loads(response.text)
+  except json.JSONDecodeError as e:
+    print(f"  Error parsing JSON from {about_url}:\n  {e}")
+    return e
 
-    return data
+  return data
 
-about = get_about("https://imag-data.bgs.ac.uk/GIN_V1/hapi/about")
-id = about['id']
-del about['status']
-del about['HAPI']
-about['contact'] = "Hello"
+def equivalent_dicts(d1, d2):
+  # Based on https://stackoverflow.com/a/10480904
+  for k1, v1 in d1.items():
+    if not k1.startswith("x_") and (k1 not in d2 or d2[k1] != v1):
+      return False
+  for k2, v2 in d2.items():
+    if k1.startswith("x_") and k2 not in d1:
+      return False
+  return True
+
+def write_legacy(fname, data):
+  print(f"Writing {fname}")
+  with open(fname, 'w') as f:
+    f.write(data)
 
 with open('../all.json') as f:
   all = json.load(f)
 
+changed = False
+all_file_str1 = ""
+all_file_str2 = ""
 for server in all['servers']:
-  if server['id'] != id:
-    continue
-  server = {**server, **about}
-  print(json.dumps(server, indent=2))
+
+  about = get_about(server['url'] + '/about') 
+  server['x_LastUpdateAttempt'] = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")
+  if isinstance(about, Exception):
+    server['x_LastUpdateError'] = str(about)
+  else:
+    server['x_LastUpdateError'] = False
+    del about["HAPI"]
+    del about["status"]
+    server_new = {**server, **about}
+    if not equivalent_dicts(server, about):
+      changed = True
+      print(f"  Difference between all.json and {server['url']}")
+      server["x_LastUpdateChange"] = server["x_LastUpdateAttempt"]
+    else:
+      print(f"  No difference between all.json and {server['url']}")
+
+  all_file_str1 += f"{server['url']}, {server['title']}, {server['id']}, {server['contact']}, {server['contactID']}\n"
+  all_file_str2 += f"{server['url']}\n"
+
+write_legacy('../all.json.new', json.dumps(all, ensure_ascii=False, separators=(',', ': '), indent=2))
+
+if changed == True:
+  write_legacy('../all_.txt.new', all_file_str1)
+  write_legacy('../all.txt.new', all_file_str2)
